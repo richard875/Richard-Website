73d0edff8f9f4e7660645d31f94b0398a8307dd9
Richard Lee <richard_875@me.com>
2022-09-04 17:52:05 +1000

    - Modify Index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,15 @@ const IndexPage = () => {
   return (
     <Container>
       <Wrapper>
-        <Top>Richard</Top>
+        <Top>
+          <div className="font-normal">hello@richard-lee.com</div>
+          <div className="font-bold">PROJECTS</div>
+        </Top>
+        <Bottom></Bottom>
       </Wrapper>
+      <Footer>
+        <FLeft>From Australia with Love | Acknowledgement of Country</FLeft>
+      </Footer>
     </Container>
   );
 };
@@ -23,19 +30,44 @@ const Container = styled.div`
   width: 100vw;
   height: 100vh;
   display: flex;
+  flex-direction: column;
   align-items: center;
-  justify-content: center;
 `;
 
 const Wrapper = styled.div`
+  margin-top: 70px;
   display: flex;
   flex-direction: column;
-  width: calc(100vw - 160px);
-  height: calc(100vh - 160px);
+  width: calc(100vw - 140px);
+  height: calc(100vh - 140px);
   border: 3px solid ${COLOR.BLACK};
+`;
+
+const Footer = styled.div`
+  width: 100%;
+  height: 70px;
+  font-size: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const FLeft = styled.div`
+  padding-left: 98px;
 `;
 
 const Top = styled.div`
   height: 80px;
+  padding-left: 28px;
+  padding-right: 28px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 30px;
   border-bottom: 3px solid ${COLOR.BLACK};
+`;
+
+const Bottom = styled.div`
+  flex: 1;
+  background: linear-gradient(90deg, #f55591 0%, #f9c41a 100%);
 `;
