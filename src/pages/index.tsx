186b168eec9fcc2b42e6092ee0d4d4f024f7bdce
Richard Lee <richard_875@me.com>
2022-09-04 18:30:19 +1000

    - Add content to Index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,10 +11,25 @@ const IndexPage = () => {
           <div className="font-normal">hello@richard-lee.com</div>
           <div className="font-bold">PROJECTS</div>
         </Top>
-        <Bottom></Bottom>
+        <Bottom>
+          <Name>RICHARD LEE</Name>
+          <Subtitle className="font-bold mt-9">
+            SOFTWARE&nbsp;ENGINEER&nbsp;&amp;&nbsp;DESIGNER
+          </Subtitle>
+          <Subtitle className="font-bold mt-5">UNIVERSITY OF SYDNEY</Subtitle>
+          <Button>Let's catch up</Button>
+        </Bottom>
       </Wrapper>
       <Footer>
-        <FLeft>From Australia with Love | Acknowledgement of Country</FLeft>
+        <FLeft>
+          <div className="mr-3">From Australia with Love</div>
+          <VerticalSeparator></VerticalSeparator>
+          <div className="ml-3">Acknowledgement of Country</div>
+        </FLeft>
+        <FRight>
+          <Indicator></Indicator>
+          <div>Sydney 9:41 am</div>
+        </FRight>
       </Footer>
     </Container>
   );
@@ -53,7 +68,29 @@ const Footer = styled.div`
 `;
 
 const FLeft = styled.div`
+  display: flex;
+  align-items: center;
   padding-left: 98px;
+`;
+
+const FRight = styled.div`
+  display: flex;
+  align-items: center;
+  padding-right: 98px;
+`;
+
+const VerticalSeparator = styled.div`
+  width: 0px;
+  height: 17px;
+  border: 1px solid ${COLOR.BLACK};
+`;
+
+const Indicator = styled.div`
+  width: 8px;
+  height: 8px;
+  margin-right: 7px;
+  border-radius: 99px;
+  background: #35be27;
 `;
 
 const Top = styled.div`
@@ -69,5 +106,33 @@ const Top = styled.div`
 
 const Bottom = styled.div`
   flex: 1;
+  padding: 95px 55px;
   background: linear-gradient(90deg, #f55591 0%, #f9c41a 100%);
+`;
+
+const Name = styled.div`
+  font-size: 100px;
+  line-height: 120px;
+  color: ${COLOR.WHITE};
+  -webkit-text-stroke: 2px ${COLOR.BLACK};
+`;
+
+const Subtitle = styled.div`
+  font-size: 30px;
+  line-height: 36px;
+  color: ${COLOR.WHITE};
+`;
+
+const Button = styled.div`
+  width: 220px;
+  height: 60px;
+  margin-top: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 23px;
+  color: ${COLOR.WHITE};
+  /* background-color: ${COLOR.BLACK}; */
+  border: 3px solid ${COLOR.WHITE};
+  cursor: pointer;
 `;
