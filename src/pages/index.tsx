ea43046c642d2fbaed0191e94f279338e59c82a5
Richard Lee <richard_875@me.com>
2022-09-11 21:44:20 +1000

    - Add animation to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import gsap from "gsap";
 import { motion } from "framer-motion";
 import { GatsbyLinkProps, Link } from "gatsby";
 import styled from "styled-components";
@@ -11,11 +12,24 @@ import { COLOR } from "../styles/theme";
 import Cursor from "../components/cursor/cursor";
 import mousePositionType from "../types/mousePositionType";
 
+const gsapAnimation = (y: number, delay: number, skewY: number) => {
+  return { y, delay, skewY, stagger: { amount: 0.3 } };
+};
+
 const IndexPage = ({
   location,
 }: {
   location: GatsbyLinkProps<mousePositionType>;
 }) => {
+  const emailRef = React.useRef(null);
+  const projectRef = React.useRef(null);
+  const acknowledgementRef = React.useRef(null);
+  const timeRef = React.useRef(null);
+  const nameRef = React.useRef(null);
+  const sub1Ref = React.useRef(null);
+  const sub2Ref = React.useRef(null);
+  const contactRef = React.useRef(null);
+
   const [hover, setHover] = React.useState(false);
   const [globalCoords, setGlobalCoords] = React.useState({
     x: 0,
@@ -31,89 +45,123 @@ const IndexPage = ({
     return () => window.removeEventListener("mousemove", handleWindowMouseMove);
   }, []);
 
+  React.useEffect(() => {
+    gsap.defaults({ ease: "power4.out", duration: 1.8 });
+    gsap.from(emailRef.current, gsapAnimation(150, 2, 20));
+    gsap.from(projectRef.current, gsapAnimation(150, 2, 20));
+    gsap.from(acknowledgementRef.current, gsapAnimation(150, 3.4, 20));
+    gsap.from(timeRef.current, gsapAnimation(150, 3.4, 20));
+    gsap.from(nameRef.current, gsapAnimation(450, 2.5, 0));
+    gsap.from(sub1Ref.current, gsapAnimation(350, 2.6, 0));
+    gsap.from(sub2Ref.current, gsapAnimation(350, 2.8, 0));
+    gsap.from(contactRef.current, gsapAnimation(350, 3.1, 0));
+  }, []);
+
   return (
     <Container
       initial={{
         opacity: 0,
-        transform: "scaleX(1.1) scaleY(1.35) translateY(-47px)",
+        transform: "scaleX(1.4) scaleY(1.75) translateY(-47px)",
       }}
-      animate={{ opacity: 1, transform: "scaleX(1) scaleY(1) translateY(0px)" }}
+      animate={{
+        opacity: 1,
+        transform: "scaleX(1) scaleY(1) translateY(0px)",
+      }}
       exit={{ opacity: 0 }}
       transition={{
         opacity: {
-          duration: 0.5,
+          duration: 1,
         },
         transform: {
-          duration: 1.5,
-          delay: 1,
+          type: "spring",
+          stiffness: 65,
+          delay: 1.5,
         },
       }}
     >
-      {/* <Cursor hover={hover} position={location.state!} isBlack={true} /> */}
+      <Cursor hover={hover} position={location.state!} isBlack={true} />
       <Wrapper>
-        <Top>
-          {/* <div
-            className="font-primary-normal"
+        <Top className="">
+          <div
+            ref={emailRef}
+            className="font-primary-normal overflow-hidden"
             onMouseEnter={() => setHover(true)}
             onMouseLeave={() => setHover(false)}
           >
             hello@richard-lee.com
           </div>
           <div
+            ref={projectRef}
             className="font-primary-bold"
             onMouseEnter={() => setHover(true)}
             onMouseLeave={() => setHover(false)}
           >
             PROJECTS
-          </div> */}
+          </div>
         </Top>
 
         <Bottom>
-          {/* <Name>RICHARD LEE</Name>
+          <Name className="font-secondary-normal">
+            <div ref={nameRef}>RICHARD LEE</div>
+          </Name>
           <Subtitle className="font-primary-bold mt-9">
-            SOFTWARE&nbsp;ENGINEER&nbsp;&amp;&nbsp;DESIGNER
+            <div ref={sub1Ref}>
+              SOFTWARE&nbsp;ENGINEER&nbsp;&amp;&nbsp;DESIGNER
+            </div>
           </Subtitle>
           <Subtitle className="font-primary-bold mt-5">
-            UNIVERSITY OF SYDNEY
+            <div ref={sub2Ref}>UNIVERSITY OF SYDNEY</div>
           </Subtitle>
-          <Button className="select-none">
-            <div
-              className="pr-2 hover:pr-3 transition-all ease-in-out underline underline-offset-4"
-              onMouseEnter={() => setHover(true)}
-              onMouseLeave={() => setHover(false)}
-            >
-              Let's catch up
+          <Button className="select-none overflow-hidden">
+            <div ref={contactRef} className="flex items-center">
+              <div
+                className="pr-2 hover:pr-3 transition-all ease-in-out underline underline-offset-4"
+                onMouseEnter={() => setHover(true)}
+                onMouseLeave={() => setHover(false)}
+              >
+                Let's catch up
+              </div>
+              <FontAwesomeIcon
+                icon={faAngleRight}
+                size={"xs"}
+                className="mt-0.5"
+              />
             </div>
-            <FontAwesomeIcon
-              icon={faAngleRight}
-              size={"xs"}
-              className="mt-0.5"
-            />
           </Button>
           <CircleContainer
             onMouseEnter={() => setHover(true)}
             onMouseLeave={() => setHover(false)}
           >
-            <Circle className="select-none">
+            <motion.div
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                duration: 0.8,
+                delay: 4,
+                ease: [0, 0.71, 0.2, 1.01],
+              }}
+            >
+              <Circle className="select-none">
+                <StaticImage
+                  className="relative h-5/6 w-5/6"
+                  src="../images/circle.svg"
+                  alt="Resume Circle"
+                  placeholder="none"
+                />
+              </Circle>
               <StaticImage
-                className="relative h-5/6 w-5/6"
-                src="../images/circle.svg"
+                width={60}
+                style={Arrow}
+                src="../images/arrow.svg"
                 alt="Resume Circle"
                 placeholder="none"
               />
-            </Circle>
-            <StaticImage
-              width={60}
-              style={Arrow}
-              src="../images/arrow.svg"
-              alt="Resume Circle"
-              placeholder="none"
-            />
-          </CircleContainer> */}
+            </motion.div>
+          </CircleContainer>
         </Bottom>
       </Wrapper>
       <Footer>
-        {/* <FLeft>
+        <FLeft ref={acknowledgementRef}>
           <div
             className="mr-3"
             onMouseEnter={() => setHover(true)}
@@ -132,10 +180,10 @@ const IndexPage = ({
             </div>
           </Link>
         </FLeft>
-        <FRight>
+        <FRight ref={timeRef}>
           <Indicator></Indicator>
           <div>Sydney 9:41 am</div>
-        </FRight> */}
+        </FRight>
       </Footer>
     </Container>
   );
@@ -155,7 +203,7 @@ const Container = styled(motion.div)`
   align-items: center;
 `;
 
-const Wrapper = styled(motion.div)`
+const Wrapper = styled.div`
   margin-top: 70px;
   display: flex;
   flex-direction: column;
@@ -164,7 +212,7 @@ const Wrapper = styled(motion.div)`
   border: 3px solid ${COLOR.BLACK};
 `;
 
-const Footer = styled(motion.div)`
+const Footer = styled.div`
   width: 100%;
   height: 70px;
   font-size: 20px;
@@ -207,6 +255,7 @@ const Top = styled.div`
   align-items: center;
   justify-content: space-between;
   font-size: 30px;
+  overflow: hidden;
   border-bottom: 3px solid ${COLOR.BLACK};
 `;
 
@@ -216,26 +265,25 @@ const Bottom = styled.div`
   background: linear-gradient(90deg, #f55591 0%, #f9c41a 100%);
 `;
 
-const Name = styled(motion.div)`
+const Name = styled.div`
   font-size: 130px;
   line-height: 120px;
+  overflow: hidden;
   color: ${COLOR.WHITE};
   -webkit-text-stroke: 0.12rem ${COLOR.BLACK};
 `;
 
-const Subtitle = styled(motion.div)`
+const Subtitle = styled.div`
   font-size: 30px;
   line-height: 36px;
+  overflow: hidden;
   color: ${COLOR.WHITE};
 `;
 
 const Button = styled.div`
-  display: flex;
-  align-items: center;
   margin-top: 50px;
   font-size: 23px;
   color: ${COLOR.WHITE};
-  cursor: pointer;
 `;
 
 const Circle = styled.div`
@@ -256,7 +304,7 @@ const Circle = styled.div`
   }
 `;
 
-const CircleContainer = styled(motion.span)`
+const CircleContainer = styled.span`
   position: absolute;
   bottom: 150px;
   right: 150px;
