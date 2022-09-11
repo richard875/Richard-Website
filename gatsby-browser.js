2a6ceb5b6734b61be456d64c5061b2ba03fceac5
Richard Lee <richard_875@me.com>
2022-09-11 17:53:47 +1000

    - Add framer motion wrapPageElement component

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,8 @@
+import React from "react";
+import { AnimatePresence } from "framer-motion";
 import "./src/styles/global.scss";
 import "./src/styles/tailwind.css";
+
+export const wrapPageElement = ({ element }) => (
+  <AnimatePresence exitBeforeEnter>{element}</AnimatePresence>
+);
