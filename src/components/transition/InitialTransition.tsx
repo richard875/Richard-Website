c3f1f72e4cbc72d28c95cae0a4c2ddb1e2aa6571
Richard Lee <richard_875@me.com>
2022-09-18 22:22:18 +1000

    - Add InitialTransition component

---
+++ src/components/transition/InitialTransition.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+import { motion } from "framer-motion";
+import styled from "styled-components";
+
+const InitialTransition = ({ color }: { color: string }) => (
+  <Transition
+    color={color}
+    initial={{
+      bottom: 0,
+      height: "0px",
+    }}
+    exit={{
+      height: "100vh",
+    }}
+    transition={{
+      duration: 1.7,
+      ease: [0.87, 0, 0.13, 1],
+    }}
+  ></Transition>
+);
+
+export default InitialTransition;
+
+const Transition = styled(motion.div)`
+  width: 100vw;
+  position: absolute;
+  z-index: 50;
+  background-color: ${(props: any) => props.color};
+`;
