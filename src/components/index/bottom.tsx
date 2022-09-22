479487219c9720edf30787b2aecc01607a256931
Richard Lee <richard_875@me.com>
2022-09-23 00:39:24 +1000

    - Add screen size support for Bottom component

--- src/components/index/bottom.tsx
+++ src/components/index/bottom.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import gsap from "gsap";
 import { motion } from "framer-motion";
 import styled from "styled-components";
+import { up, down, between } from "styled-breakpoints";
+import { useBreakpoint } from "styled-breakpoints/react-styled";
 import { StaticImage } from "gatsby-plugin-image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
@@ -13,33 +15,58 @@ const Bottom = ({
 }: {
   setHover: (value: React.SetStateAction<boolean>) => void;
 }) => {
+  const topNameRef = React.useRef(null);
+  const topGreetingRef = React.useRef(null);
   const nameRef = React.useRef(null);
   const sub1Ref = React.useRef(null);
   const sub2Ref = React.useRef(null);
   const contactRef = React.useRef(null);
+  const countryRef = React.useRef(null);
+  const mobileCountryRef = React.useRef(null);
 
   React.useEffect(() => {
     gsap.defaults({ ease: "power4.out", duration: 1.8 });
+    gsap.from(topNameRef.current, gsapAnimationIndex(450, 2.2, 0));
+    gsap.from(topGreetingRef.current, gsapAnimationIndex(450, 2.2, 0));
     gsap.from(nameRef.current, gsapAnimationIndex(450, 2.5, 0));
     gsap.from(sub1Ref.current, gsapAnimationIndex(350, 2.6, 0));
     gsap.from(sub2Ref.current, gsapAnimationIndex(350, 2.8, 0));
     gsap.from(contactRef.current, gsapAnimationIndex(350, 3.1, 0));
+    gsap.from(countryRef.current, gsapAnimationIndex(350, 3.3, 0));
+    gsap.from(mobileCountryRef.current, gsapAnimationIndex(350, 3.5, 0));
   }, []);
 
   return (
     <Container>
+      <SmalllText>
+        <div ref={topNameRef}>
+          {useBreakpoint(down("sm")) && (
+            <div className="flex items-center justify-between font-primary-bold mt-3 mb-8">
+              <div>hello@richard-lee.com</div>
+              <div>PROJECTS</div>
+            </div>
+          )}
+        </div>
+      </SmalllText>
+      <SmalllText>
+        <div ref={topGreetingRef}>
+          {useBreakpoint(up("sm")) && (
+            <div className="font-primary-bold mt-4 mb-6">
+              From Australia with Love
+            </div>
+          )}
+        </div>
+      </SmalllText>
       <Name className="font-secondary-normal">
         <div ref={nameRef}>RICHARD LEE</div>
       </Name>
-      <Subtitle className="font-primary-bold mt-9">
-        <div ref={sub1Ref}>
-          SOFTWARE&nbsp;ENGINEER&nbsp;&amp;&nbsp;CREATIVE DESIGNER
-        </div>
+      <Subtitle className="font-primary-bold mt-5 sm:mt-8">
+        <div ref={sub1Ref}>SOFTWARE ENGINEER &amp; CREATIVE DESIGNER</div>
       </Subtitle>
-      <Subtitle className="font-primary-bold mt-5">
+      <Subtitle className="font-primary-bold mt-2 sm:mt-4">
         <div ref={sub2Ref}>UNIVERSITY OF SYDNEY</div>
       </Subtitle>
-      <Button className="select-none overflow-hidden">
+      <Button>
         <div ref={contactRef} className="flex items-center">
           <div
             className="pr-2 hover:pr-3 transition-all ease-in-out underline underline-offset-4"
@@ -51,6 +78,19 @@ const Bottom = ({
           <FontAwesomeIcon icon={faAngleRight} size={"xs"} className="mt-0.5" />
         </div>
       </Button>
+      <Country>
+        <div ref={countryRef}>
+          {useBreakpoint(down("sm")) && <>From Australia with Love</>}
+          {useBreakpoint(between("sm", "lg")) && (
+            <>Acknowledgement of Country</>
+          )}
+        </div>
+      </Country>
+      <Acknowledgement>
+        <div ref={mobileCountryRef}>
+          {useBreakpoint(down("sm")) && <>Acknowledgement of Country</>}
+        </div>
+      </Acknowledgement>
       <CircleContainer
         onMouseEnter={() => setHover(true)}
         onMouseLeave={() => setHover(false)}
@@ -64,7 +104,7 @@ const Bottom = ({
             ease: [0, 0.71, 0.2, 1.01],
           }}
         >
-          <Circle className="select-none">
+          <Circle>
             <StaticImage
               className="relative h-5/6 w-5/6"
               src="../../images/circle.png"
@@ -72,13 +112,45 @@ const Bottom = ({
               placeholder="none"
             />
           </Circle>
-          <StaticImage
-            width={60}
-            style={Arrow}
-            src="../../images/arrow.svg"
-            alt="Resume Circle"
-            placeholder="none"
-          />
+          {useBreakpoint(down("sm")) && (
+            <StaticImage
+              width={45}
+              style={{
+                position: "absolute",
+                left: 42.5,
+                bottom: 56,
+              }}
+              src="../../images/arrow.svg"
+              alt="Resume Circle"
+              placeholder="none"
+            />
+          )}
+          {useBreakpoint(between("sm", "lg")) && (
+            <StaticImage
+              width={50}
+              style={{
+                position: "absolute",
+                left: 50,
+                bottom: 65,
+              }}
+              src="../../images/arrow.svg"
+              alt="Resume Circle"
+              placeholder="none"
+            />
+          )}
+          {useBreakpoint(up("lg")) && (
+            <StaticImage
+              width={60}
+              style={{
+                position: "absolute",
+                left: 55,
+                bottom: 72,
+              }}
+              src="../../images/arrow.svg"
+              alt="Resume Circle"
+              placeholder="none"
+            />
+          )}
         </motion.div>
       </CircleContainer>
     </Container>
@@ -89,34 +161,114 @@ export default Bottom;
 
 const Container = styled.div`
   flex: 1;
-  padding: 95px 55px;
+  padding: 15px 12px;
   background: linear-gradient(90deg, #f55591 0%, #f9c41a 100%);
+
+  ${up("sm")} {
+    padding: 30px 30px;
+  }
+
+  ${up("lg")} {
+    padding: 10px 30px;
+  }
+
+  ${up("xxl")} {
+    padding: 3vh 3vw;
+  }
+`;
+
+const SmalllText = styled.div`
+  color: ${COLOR.WHITE};
+  overflow: hidden;
+
+  ${up("sm")} {
+    font-size: 20px;
+  }
+
+  ${up("xxl")} {
+    font-size: 25px;
+  }
 `;
 
 const Name = styled.div`
-  font-size: 130px;
-  line-height: 120px;
+  font-size: 73px;
+  line-height: 70px;
   overflow: hidden;
   color: ${COLOR.WHITE};
   -webkit-text-stroke: 0.12rem ${COLOR.BLACK};
+
+  ${up("sm")} {
+    font-size: 80px;
+    line-height: 80px;
+  }
+
+  ${up("lg")} {
+    font-size: 100px;
+    line-height: 100px;
+  }
+
+  ${up("xxl")} {
+    margin-top: 1vw;
+    font-size: 130px;
+    line-height: 120px;
+  }
 `;
 
 const Subtitle = styled.div`
-  font-size: 30px;
-  line-height: 36px;
+  line-height: 18px;
   overflow: hidden;
   color: ${COLOR.WHITE};
+
+  ${up("sm")} {
+    font-size: 23px;
+    line-height: 30px;
+  }
+
+  ${up("xxl")} {
+    font-size: 30px;
+    line-height: 36px;
+  }
 `;
 
 const Button = styled.div`
   margin-top: 50px;
-  font-size: 23px;
   color: ${COLOR.WHITE};
+  user-select: none;
+  overflow: hidden;
+
+  ${up("sm")} {
+    font-size: 20px;
+  }
+
+  ${up("xxl")} {
+    font-size: 23px;
+  }
+`;
+
+const Country = styled.div`
+  font-size: 14px;
+  position: absolute;
+  bottom: 16vh;
+  color: ${COLOR.WHITE};
+  overflow: hidden;
+
+  ${up("sm")} {
+    font-size: 17px;
+    bottom: 8vh;
+  }
+`;
+
+const Acknowledgement = styled.div`
+  font-size: 14px;
+  position: absolute;
+  bottom: 13vh;
+  color: ${COLOR.WHITE};
+  overflow: hidden;
 `;
 
 const Circle = styled.div`
-  width: 170px;
-  height: 170px;
+  width: 130px;
+  height: 130px;
   border-radius: 99px;
   display: flex;
   align-items: center;
@@ -130,21 +282,41 @@ const Circle = styled.div`
       transform: rotate(-360deg);
     }
   }
+
+  ${up("sm")} {
+    width: 150px;
+    height: 150px;
+  }
+
+  ${up("lg")} {
+    width: 170px;
+    height: 170px;
+  }
 `;
 
 const CircleContainer = styled.span`
   position: absolute;
-  bottom: 150px;
-  right: 150px;
+  bottom: 16vh;
+  right: 30px;
   transition: 0.5s all cubic-bezier(0.045, 0.32, 0.265, 1);
+  user-select: none;
 
   &:hover {
     transform: rotate(-360deg) scale(1.07);
   }
-`;
 
-const Arrow = {
-  position: "absolute",
-  left: 55,
-  bottom: 72,
-} as React.CSSProperties;
+  ${up("sm")} {
+    bottom: 10vh;
+    right: 35px;
+  }
+
+  ${up("lg")} {
+    bottom: 110px;
+    right: 50px;
+  }
+
+  ${up("xxl")} {
+    bottom: 15vh;
+    right: calc(150px - 3.5vw);
+  }
+`;
