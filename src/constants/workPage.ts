682ab71e53d25da39eda5127eee5a54120fb3537
Richard Lee <richard_875@me.com>
2023-02-06 21:19:19 +1100

    - Refactor: Add workPage constant variables

---
+++ src/constants/workPage.ts
@@ -0,0 +1,5 @@
+export const BLOCK_PADDING = 10;
+export const BLOCK_PADDING_DESKTOP = 25;
+export const BLOCK_WIDTH = 500;
+export const BLOCK_WIDTH_DESKTOP = 580;
+export const IMAGE_DEFAULT_HEIGHT = 50;
