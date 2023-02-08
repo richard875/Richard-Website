91fe949cf59ed8b122a7fb74ab6fb0dca72fb89a
Richard Lee <richard_875@me.com>
2023-02-08 23:21:00 +1100

    - Feature: Add page transition to the work page

--- src/components/work/callToAction.tsx
+++ src/components/work/callToAction.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
+import { navigate } from "gatsby";
 import styled from "styled-components";
+import Route from "../../routes/route";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleChevronRight } from "@fortawesome/free-solid-svg-icons";
 import { COLOR } from "../../styles/theme";
@@ -9,13 +11,22 @@ const CallToAction = ({
 }: {
   setHover: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
+  const projects = (event: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
+    event.preventDefault();
+    document.body.style.backgroundColor = COLOR.BACKGROUND_WHITE_SECONDARY;
+    navigate(Route.Projects, { state: { x: event.clientX, y: event.clientY } });
+  };
+
   return (
     <Cta
       className="font-secondary-normal"
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
     >
-      <div className="pr-1.5 hover:pr-3 transition-all ease-in-out underline underline-offset-2">
+      <div
+        onClick={(e) => projects(e)}
+        className="pr-1.5 hover:pr-3 transition-all ease-in-out underline underline-offset-2"
+      >
         Projects
       </div>
       <FontAwesomeIcon
