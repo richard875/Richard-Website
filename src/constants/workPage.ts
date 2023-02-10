e0ab5ec997c2f51debfd874c9f7f4d51156ce5a9
Richard Lee <richard_875@me.com>
2023-02-11 03:01:20 +1100

    - Refactor: Remove unneeded constants

--- src/constants/workPage.ts
+++ src/constants/workPage.ts
@@ -3,5 +3,3 @@ export const BLOCK_PADDING_DESKTOP = 25;
 export const BLOCK_WIDTH = 500;
 export const BLOCK_WIDTH_DESKTOP = 580;
 export const IMAGE_DEFAULT_HEIGHT = 50;
-export const MEDIA_TOP_OFFSET = 225;
-export const MEDIA_TOP_OFFSET_DESKTOP = 245;
