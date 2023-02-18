3dae845c9a6ebed827915efb4aa2ad24af4995a7
Richard Lee <richard_875@me.com>
2023-02-18 22:25:52 +1100

    - Refactor: Add global CallToAction component

---
+++ src/components/global/callToAction.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { COLOR } from "../../styles/theme";
+
+const CallToAction = ({
+  name,
+  setHover,
+  isDarkMode,
+  navigator,
+}: {
+  name: string;
+  setHover: React.Dispatch<React.SetStateAction<boolean>>;
+  isDarkMode: boolean;
+  navigator: (event: React.MouseEvent<HTMLSpanElement, MouseEvent>) => void;
+}) => {
+  return (
+    <Cta
+      isDarkMode={isDarkMode}
+      className="font-secondary-normal"
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      <div
+        onClick={(e) => navigator(e)}
+        className="pr-1.5 hover:pr-3 transition-all ease-in-out underline underline-offset-2"
+      >
+        {name}
+      </div>
+      <FontAwesomeIcon
+        icon={faCircleChevronRight}
+        size={"sm"}
+        className="mt-1"
+      />
+    </Cta>
+  );
+};
+
+export default CallToAction;
+
+const Cta = styled.div`
+  display: flex;
+  align-items: center;
+  color: ${({ isDarkMode }: { isDarkMode: boolean }) =>
+    isDarkMode ? COLOR.BRIGHT_GREEN : COLOR.DIM_GREEN};
+`;
