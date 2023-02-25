dd2f3b0080ea5ec973e576a71e48862cee3d61dc
Richard Lee <richard_875@me.com>
2023-02-25 15:27:18 +1100

    - Feature: Add initial background color

--- src/components/transition/InitialTransition.tsx
+++ src/components/transition/InitialTransition.tsx
@@ -2,28 +2,32 @@ import * as React from "react";
 import { motion } from "framer-motion";
 import styled from "styled-components";
 
-const InitialTransition = ({ color }: { color: string }) => (
-  <Transition
-    color={color}
-    initial={{
-      bottom: 0,
-      height: "0px",
-    }}
-    exit={{
-      height: "100vh",
-    }}
-    transition={{
-      duration: 1.7,
-      ease: [0.87, 0, 0.13, 1],
-    }}
-  ></Transition>
-);
+const InitialTransition = ({ color }: { color: string }) => {
+  React.useEffect(() => {
+    document.body.style.backgroundColor = color;
+  }, []);
+
+  return (
+    <Transition
+      color={color}
+      initial={{
+        bottom: 0,
+        height: "0px",
+      }}
+      exit={{ height: "100vh" }}
+      transition={{
+        duration: 1.7,
+        ease: [0.87, 0, 0.13, 1],
+      }}
+    ></Transition>
+  );
+};
 
 export default InitialTransition;
 
 const Transition = styled(motion.div)`
   width: 100vw;
   position: absolute;
-  z-index: 50;
+  z-index: 99999;
   background-color: ${(props: any) => props.color};
 `;
