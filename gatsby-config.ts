316c255258476d1a0bb6e74c79c4b87ea11fbb7b
Richard Lee <richard_875@me.com>
2023-02-26 00:38:28 +1100

    - Config: Modify siteMetadata

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -6,8 +6,8 @@ require("dotenv").config({
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `richard-website`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `Richard Lee | Software Engineer | University of Sydney`,
+    siteUrl: `https://richard-lee.com`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
