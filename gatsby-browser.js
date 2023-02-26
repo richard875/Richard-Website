78e7d62951e08e8e8dfbb4042094ac82c4790f83
Richard Lee <richard_875@me.com>
2023-02-26 15:27:38 +1100

    - Feature: Show update prompt on PWA mode

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,18 +2,25 @@ import React from "react";
 import { AnimatePresence } from "framer-motion";
 import "./src/styles/global.scss";
 import "./src/styles/tailwind.css";
+import { MODE, STANDALONE } from "./src/constants/meta";
 
 export const wrapPageElement = ({ element }) => (
   <AnimatePresence mode="wait">{element}</AnimatePresence>
 );
 
 export const onServiceWorkerUpdateReady = () => {
-  const answer = window.confirm(
-    `This application has been updated. ` +
-      `Reload to display the latest version?`
-  );
+  // Detect if the page is opened as a PWA
+  const params = new URLSearchParams(location.search);
+  const isPwa = params.get(MODE) === STANDALONE;
 
-  if (answer === true) {
-    window.location.reload();
+  if (isPwa) {
+    const answer = window.confirm(
+      `This application has been updated. ` +
+        `Reload to display the latest version?`
+    );
+
+    if (answer === true) {
+      window.location.reload();
+    }
   }
 };
