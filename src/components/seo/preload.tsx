d112aa291049f44d696891d80277870a00429a71
Richard Lee <richard_875@me.com>
2023-11-25 15:16:04 +1100

    - Add new videos to preload

--- src/components/seo/preload.tsx
+++ src/components/seo/preload.tsx
@@ -3,10 +3,12 @@ import fontPrimaryNormal from "../../../static/fonts/SansSerifFLF-Demibold.woff"
 import fontPrimaryBold from "../../../static/fonts/SansSerifBldFLF.woff";
 import fontSecondaryNormal from "../../../static/fonts/BwGradual-Medium.ttf";
 import fontSecondaryBold from "../../../static/fonts/sf-pro-medium.otf";
-import smh from "../../../static/videos/smh.mp4";
+import canary from "../../../static/videos/canary.mp4";
 import cie from "../../../static/videos/cie.mp4";
+import maily from "../../../static/videos/maily.mp4";
 import neetcode from "../../../static/videos/neetcode.mp4";
 import piston from "../../../static/videos/piston.mp4";
+import smh from "../../../static/videos/smh.mp4";
 
 const Preload = () => {
   return (
@@ -42,10 +44,12 @@ const Preload = () => {
       />
 
       {/* Preload Content */}
+      <link rel="preload" href={canary} as="video" type="video/mp4" />
+      <link rel="preload" href={cie} as="video" type="video/mp4" />
+      <link rel="preload" href={maily} as="video" type="video/mp4" />
       <link rel="preload" href={neetcode} as="video" type="video/mp4" />
       <link rel="preload" href={piston} as="video" type="video/mp4" />
       <link rel="preload" href={smh} as="video" type="video/mp4" />
-      <link rel="preload" href={cie} as="video" type="video/mp4" />
     </>
   );
 };
