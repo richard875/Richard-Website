ee622f893918ec6120c8e97329a37bd0b0e5202d
Richard Lee <richard_875@me.com>
2023-11-25 14:32:14 +1100

    - Add canary and maily videos

--- src/helper/mediaPicker.ts
+++ src/helper/mediaPicker.ts
@@ -1,18 +1,24 @@
+import canary from "../../static/videos/canary.mp4";
 import cie from "../../static/videos/cie.mp4";
-import smh from "../../static/videos/smh.mp4";
+import maily from "../../static/videos/maily.mp4";
 import neetcode from "../../static/videos/neetcode.mp4";
 import piston from "../../static/videos/piston.mp4";
+import smh from "../../static/videos/smh.mp4";
 
 const mediaPicker = (media: string) => {
   switch (media) {
+    case "canary":
+      return canary;
     case "cie":
       return cie;
-    case "smh":
-      return smh;
+    case "maily":
+      return maily;
     case "neetcode":
       return neetcode;
     case "piston":
       return piston;
+    case "smh":
+      return smh;
     default:
       return "";
   }
