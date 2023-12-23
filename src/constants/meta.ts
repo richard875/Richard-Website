8a010df3e75448171949ff424b65804f38c6654b
Richard Lee <richard_875@me.com>
2023-12-23 22:50:45 +1100

    - Refactor: Add constants exports

--- src/constants/meta.ts
+++ src/constants/meta.ts
@@ -1,17 +1,30 @@
+// Site Meta
 export const NAME = "Richard Lee";
 export const URL = process.env.GATSBY_SITE_URL;
 export const EMAIL = `hello@${URL}`;
-export const LINKEDIN_URL = "https://www.linkedin.com/in/richard875";
 export const GITHUB_URL = "https://github.com/richard875";
+export const LINKEDIN_URL = "https://www.linkedin.com/in/richard875";
+
+// Site Config
 export const MODE = "mode";
 export const STANDALONE = "standalone";
 export const STANDALONE_URL = `?${MODE}=${STANDALONE}`;
 export const ORGANIZATION = process.env.GATSBY_ORGANIZATION;
 export const COPYRIGHT = `© ${new Date().getFullYear()} ${NAME}`;
 
-export const SITE_TITLE = `${NAME} | Software Engineer | Sydney, Australia`;
-export const PAGE_TITLE = ` | ${NAME} | Software Engineer`;
-export const SITE_DESCRIPTION =
-  "G'day, I'm Richard. I'm a Software Engineer and Creative Designer from Sydney, Australia. On this corner of the internet, you'll find information about me.";
+// Site Title and Description
+export const REGION = "Sydney, Australia";
+export const OCCUPATION = "Software Engineer";
+export const PAGE_TITLE = ` | ${NAME} | ${OCCUPATION}`;
+export const SITE_TITLE = `${NAME} | ${OCCUPATION} | ${REGION}`;
+export const SITE_DESCRIPTION = `G'day, I'm Richard. I'm a ${OCCUPATION} and Creative Designer from ${REGION}. On this corner of the internet, you'll find information about me.`;
 export const SITE_KEYWORDS =
   "Richard, Lee, University, Sydney, Software, Engineering, React, Angular, postgresql, AWS, Amazon Web Services, SQL, Database, HTML, CSS, JavaScript, Programming, coding, api, Oracle, Agile, Student, Problem solving, Collaboration, Communication, C#, .NET, ASP, Microsoft, Google, Apple, Computer Science, Science, Australia, Motivated, Linkedin, New South Wales";
+
+// Page Titles
+export const INTRO_TITLE = "G'day";
+export const PROJECTS_TITLE = "Projects";
+export const CONTACT_TITLE = "Contact Me";
+export const EDUCATION_TITLE = "Education";
+export const ACKNOWLEDGEMENT_TITLE = "Acknowledgement";
+export const EXPERIENCE_TITLE = "Skills & Work Experience";
