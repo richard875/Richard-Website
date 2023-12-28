a906435c8d3b24980fa4785d993d4ddde5a75ea4
Richard Lee <richard_875@me.com>
2023-12-28 16:29:27 +1100

    - Style: Apply circle size for index

--- src/components/index/bottom.tsx
+++ src/components/index/bottom.tsx
@@ -40,43 +40,51 @@ const Bottom = ({
 
   return (
     <Container>
-      <SmallText>
-        <h3 ref={topGreetingRef} className="font-primary-normal mt-4 mb-6">
-          From Australia with Love
-        </h3>
-      </SmallText>
-      <Name className="font-primary-bold">
-        <h1 ref={nameRef}>RICHARD LEE</h1>
-      </Name>
-      <SmallText className="font-primary-normal mt-5 sm:mt-8">
-        <h2 ref={sub1Ref}>Software Engineer &amp; Creative Designer</h2>
-      </SmallText>
-      <SmallText className="font-primary-normal mt-1/2 sm:mt-1">
-        <h2 ref={sub2Ref}>Sydney, Australia</h2>
-      </SmallText>
-      <Button id={`${INDEX_TO_INTRO}_0`}>
-        <div
-          ref={contactRef}
-          id={`${INDEX_TO_INTRO}_1`}
-          className="flex items-center font-secondary-normal"
-        >
-          <h2
-            id={`${INDEX_TO_INTRO}_2`}
-            className="pr-1.5 hover:pr-3 transition-all ease-in-out underline underline-offset-2"
-            onClick={(e) => routeTo(e, Route.Intro)}
-            onMouseEnter={() => setHover(true)}
-            onMouseLeave={() => setHover(false)}
+      <div>
+        <SmallText>
+          <h3 ref={topGreetingRef} className="font-primary-normal mt-4 mb-6">
+            From Australia with Love
+          </h3>
+        </SmallText>
+        <Name className="font-primary-bold">
+          <h1 ref={nameRef}>RICHARD LEE</h1>
+        </Name>
+        <SmallText className="font-primary-normal mt-5 sm:mt-8">
+          <h2 ref={sub1Ref}>Software Engineer &amp; Creative Designer</h2>
+        </SmallText>
+        <SmallText className="font-primary-normal mt-1/2 sm:mt-1">
+          <h2 ref={sub2Ref}>Sydney, Australia</h2>
+        </SmallText>
+        <Button id={`${INDEX_TO_INTRO}_0`}>
+          <div
+            ref={contactRef}
+            id={`${INDEX_TO_INTRO}_1`}
+            className="flex items-center font-secondary-normal"
           >
-            My experience
-          </h2>
-          <FontAwesomeIcon
-            id={`${INDEX_TO_INTRO}_3`}
-            icon={faCircleChevronRight}
-            className="mt-1"
-            size={"sm"}
-          />
-        </div>
-      </Button>
+            <h2
+              id={`${INDEX_TO_INTRO}_2`}
+              className="pr-1.5 hover:pr-3 transition-all ease-in-out underline underline-offset-2"
+              onClick={(e) => routeTo(e, Route.Intro)}
+              onMouseEnter={() => setHover(true)}
+              onMouseLeave={() => setHover(false)}
+            >
+              My experience
+            </h2>
+            <FontAwesomeIcon
+              id={`${INDEX_TO_INTRO}_3`}
+              icon={faCircleChevronRight}
+              className="mt-1"
+              size={"sm"}
+            />
+          </div>
+        </Button>
+      </div>
+      {Array.from({ length: 6 }, (_, i) => (
+        <div key={i}></div>
+      ))}
+      <div className="flex justify-end mr-[7vw] mb-3 sm:mr-0 sm:mb-0 lg:mr-5 lg:mb-5">
+        <ResumeCircle isHome={true} setHover={setHover} />
+      </div>
       <Country id={`${INDEX_TO_ACKNOWLEDGEMENT_MOBILE}_0`}>
         <h2
           ref={countryRef}
@@ -89,7 +97,6 @@ const Bottom = ({
           {!isIphoneXPwa && "Acknowledgement of Country"}
         </h2>
       </Country>
-      <ResumeCircle isHome={true} setHover={setHover} />
     </Container>
   );
 };
@@ -98,6 +105,9 @@ export default Bottom;
 
 const Container = styled.div`
   flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   padding: 15px 12px;
   background: linear-gradient(90deg, #f55591 0%, #f9c41a 100%);
 
@@ -106,7 +116,7 @@ const Container = styled.div`
   }
 
   @media ${layout.up.lg} {
-    padding: 3vh 3vw;
+    padding: 3vh 3vw 3vw 3vw;
   }
 `;
 
@@ -166,14 +176,11 @@ const Button = styled.div`
 `;
 
 const Country = styled.div`
-  bottom: 1.5vh;
   font-size: 16px;
   overflow: hidden;
-  position: absolute;
   color: ${Color.WHITE};
 
   @media ${layout.up.sm} {
-    bottom: 8vh;
     font-size: 17px;
   }
 
