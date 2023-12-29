43719ca5716bc365578964fac7126a6d326f6cd6
Richard Lee <richard_875@me.com>
2023-12-29 23:25:03 +1100

    - Feature: Add logging table helper

---
+++ src/helper/table.ts
@@ -0,0 +1,27 @@
+import { Console } from "console";
+import { Transform } from "stream";
+
+const table = (input) => {
+  // @see https://stackoverflow.com/a/67859384
+  const ts = new Transform({
+    transform(chunk, enc, cb) {
+      cb(null, chunk);
+    },
+  });
+  const logger = new Console({ stdout: ts });
+  logger.table(input);
+  const table = (ts.read() || "").toString();
+  let result = "\r\n";
+  for (let row of table.split(/[\r\n]+/)) {
+    let r = row.replace(/[^┬]*┬/, "┌");
+    r = r.replace(/^├─*┼/, "├");
+    r = r.replace(/│[^│]*/, "");
+    r = r.replace(/^└─*┴/, "└");
+    r = r.replace(/'/g, " ");
+    result += `${r}\n`;
+  }
+
+  return result;
+};
+
+export default table;
