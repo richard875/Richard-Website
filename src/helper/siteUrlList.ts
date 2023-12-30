998f72a1f132707b39e4a0be1ca7e5954c18eaec
Richard Lee <richard_875@me.com>
2023-12-30 22:42:49 +1100

    - Config: Remove trailing slash from Index Page

--- src/helper/siteUrlList.ts
+++ src/helper/siteUrlList.ts
@@ -3,7 +3,7 @@ import { HTTPS } from "../constants/meta";
 
 const SITE_URL_HTTPS = `${HTTPS}${process.env.GATSBY_SITE_URL!}`;
 const urlList = Object.values(Route)
-  .filter((route) => route !== Route.NotFound)
-  .map((route) => `${SITE_URL_HTTPS}${route}`);
+  .filter((path) => path !== Route.NotFound)
+  .map((path) => `${SITE_URL_HTTPS}${path === Route.Home ? "" : path}`);
 
 export default urlList;
