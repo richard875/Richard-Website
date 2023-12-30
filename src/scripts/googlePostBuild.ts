023a3a29bf5592342fda0940f8a80eafac5360d8
Richard Lee <richard_875@me.com>
2023-12-30 23:22:26 +1100

    - Test: Trigger Re-deploy

--- src/scripts/googlePostBuild.ts
+++ src/scripts/googlePostBuild.ts
@@ -7,8 +7,10 @@ import axioConfigGoogle from "../helper/axioConfigGoogle";
 
 const googlePostBuild = async (reporter: Reporter) => {
   // Variables
+  reporter.info("here 1");
   const client_email = process.env.GOOGLE_CLIENT_EMAIL!;
   const private_key = process.env.GOOGLE_PRIVATE_KEY!;
+  reporter.info("here 2");
 
   const jwtClient = new google.auth.JWT(
     client_email,
