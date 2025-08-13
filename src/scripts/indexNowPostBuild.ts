96403b59330df6b4b8b2de8b9d893b88000354c0
Richard Everley <hello@richardeverley.com>
2025-08-14 03:21:09 +1000

    - Feature: Refactor `indexNowPostBuild` and add Indexing for Yep Search Engine

--- src/scripts/indexNowPostBuild.ts
+++ src/scripts/indexNowPostBuild.ts
@@ -1,44 +1,28 @@
 import fs from "fs";
-import axios from "axios";
 import { Reporter } from "gatsby";
-import IndexNow from "../types/indexNow";
-import { HTTPS } from "../constants/meta";
-import indexNowDoc from "../constants/indexNowDoc";
 import table from "../helper/table";
-import urlList from "../helper/siteUrlList";
-import axioIdxNow from "../helper/axioConfigIndexNow";
+import safeRequest from "./safeRequest";
+import indexNowDoc from "../constants/indexNowDoc";
 
 const INDEXNOW_URL = "api.indexnow.org";
 const BING_URL = "www.bing.com";
 const NAVER_URL = "searchadvisor.naver.com";
 const SEZNAM_URL = "search.seznam.cz";
 const YANDEX_URL = "yandex.com";
+const YEP_URL = "indexnow.yep.com";
 
 const indexNowPostBuild = async (reporter: Reporter) => {
-  // Variables
-  const indexNowKey = process.env.INDEXNOW_KEY!;
-  const SITE_URL = process.env.GATSBY_SITE_URL!;
-  const SITE_URL_HTTPS = `${HTTPS}${process.env.GATSBY_SITE_URL!}`;
-
   // Write File to Public Folder
-  const fileName = `${indexNowKey}.txt`;
-  const filePath = `./public/${fileName}`;
-  fs.writeFileSync(filePath, indexNowKey);
+  const filePath = `./public/${process.env.INDEXNOW_KEY!}.txt`;
+  fs.writeFileSync(filePath, process.env.INDEXNOW_KEY!);
 
-  // Construct Data
-  const data: IndexNow = {
-    host: SITE_URL,
-    key: indexNowKey,
-    keyLocation: `${SITE_URL_HTTPS}/${fileName}`,
-    urlList: urlList,
-  };
-
-  // Request
-  const indexNowResponse = await axios.request(axioIdxNow(INDEXNOW_URL, data));
-  const bingResponse = await axios.request(axioIdxNow(BING_URL, data));
-  const naverResponse = await axios.request(axioIdxNow(NAVER_URL, data));
-  const seznamResponse = await axios.request(axioIdxNow(SEZNAM_URL, data));
-  const yandexResponse = await axios.request(axioIdxNow(YANDEX_URL, data));
+  // Requests
+  const indexNowResponse = await safeRequest(INDEXNOW_URL, reporter);
+  const bingResponse = await safeRequest(BING_URL, reporter);
+  const naverResponse = await safeRequest(NAVER_URL, reporter);
+  const seznamResponse = await safeRequest(SEZNAM_URL, reporter);
+  const yandexResponse = await safeRequest(YANDEX_URL, reporter);
+  const yepResponse = await safeRequest(YEP_URL, reporter);
 
   // Log Response
   reporter.info("------------------IndexNow Response------------------");
@@ -51,9 +35,11 @@ const indexNowPostBuild = async (reporter: Reporter) => {
   reporter.info(`${seznamResponse.status} ${seznamResponse.statusText}`);
   reporter.info("-------------------Yandex Response-------------------");
   reporter.info(`${yandexResponse.status} ${yandexResponse.statusText}`);
+  reporter.info("-------------------Yep Response-------------------");
+  reporter.info(`${yepResponse.status} ${yepResponse.statusText}`);
 
   reporter.info("----------------IndexNow Status Chart----------------");
-  reporter.info(table(indexNowDoc));
+  reporter.info(`\n${table(indexNowDoc)}`);
 };
 
 export default indexNowPostBuild;
