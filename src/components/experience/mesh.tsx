84cff55fc9393fc30db6e6192bd52edc6fb97196
Richard Everley <hello@richardeverley.com>
2026-06-28 20:04:29 +1000

    feat: Use random boat position

--- src/components/experience/mesh.tsx
+++ src/components/experience/mesh.tsx
@@ -25,6 +25,53 @@ const Mesh = () => {
     [materials.Glass],
   );
 
+  const boat1Position = React.useMemo(
+    () =>
+      [
+        Math.floor(Math.random() * (-1200 - -4200 + 1)) + -4200,
+        -50,
+        Math.floor(Math.random() * (3000 - 1600 + 1)) + 1600,
+      ] as [number, number, number],
+    [],
+  );
+
+  const boat2Position = React.useMemo(
+    () =>
+      [
+        Math.floor(Math.random() * (1200 - -4200 + 1)) + -4200,
+        -40,
+        Math.floor(Math.random() * (-4700 - -6300 + 1)) + -6300,
+      ] as [number, number, number],
+    [],
+  );
+
+  const boat3Position = React.useMemo(
+    () =>
+      [
+        Math.floor(Math.random() * (2000 - -800 + 1)) + -800,
+        -50,
+        Math.floor(Math.random() * (3000 - 2000 + 1)) + 2000,
+      ] as [number, number, number],
+    [],
+  );
+
+  const boat1Rotation = React.useMemo(
+    () => [0.1, Math.random() * Math.PI * 2, -0.07] as [number, number, number],
+    [],
+  );
+
+  const boat2Rotation = React.useMemo(
+    () =>
+      [0.26, Math.random() * Math.PI * 2, -0.15] as [number, number, number],
+    [],
+  );
+
+  const boat3Rotation = React.useMemo(
+    () =>
+      [3.02, Math.random() * Math.PI * 2, -3.01] as [number, number, number],
+    [],
+  );
+
   return (
     <group position={[0, 0.6, 0]} dispose={null}>
       <group rotation={[-Math.PI / 2, 0, 0]} scale={0.0003}>
@@ -679,9 +726,15 @@ const Mesh = () => {
             position={[-1746.98, 461.43, -538.72]}
             rotation={[0, -Math.PI / 2, 0]}
           >
-            <group
-              position={[-2233.02, -58.99, 2595.18]}
-              rotation={[0.1, 1.06, -0.07]}
+            <AnimatedBoat
+              position={boat1Position}
+              rotation={boat1Rotation}
+              speed={1.1}
+              amplitude={180}
+              drift={120}
+              phase={0.4}
+              boatLength={80}
+              boatWidth={20}
             >
               <mesh
                 castShadow
@@ -699,10 +752,17 @@ const Mesh = () => {
                 geometry={(nodes.Yacht_Glass_0 as THREE.Mesh).geometry}
                 material={glassMaterial}
               />
-            </group>
-            <group
-              position={[-3187.63, -47.83, -6281.06]}
-              rotation={[0.26, 1.49, -0.15]}
+            </AnimatedBoat>
+            <AnimatedBoat
+              position={boat2Position}
+              rotation={boat2Rotation}
+              speed={1.3}
+              amplitude={160}
+              drift={110}
+              phase={1.8}
+              boatLength={70}
+              boatWidth={18}
+              orientationOffset={[0, Math.PI / 2, 0]}
             >
               <mesh
                 castShadow
@@ -714,10 +774,16 @@ const Mesh = () => {
                 geometry={(nodes.Yacht_4_Glass_0 as THREE.Mesh).geometry}
                 material={glassMaterial}
               />
-            </group>
-            <group
-              position={[2146.79, -58.99, 2807.03]}
-              rotation={[3.02, 1.15, -3.01]}
+            </AnimatedBoat>
+            <AnimatedBoat
+              position={boat3Position}
+              rotation={boat3Rotation}
+              speed={0.95}
+              amplitude={200}
+              drift={130}
+              phase={2.4}
+              boatLength={75}
+              boatWidth={22}
             >
               <mesh
                 castShadow
@@ -735,7 +801,7 @@ const Mesh = () => {
                 geometry={(nodes.Yacht_1_Glass_0 as THREE.Mesh).geometry}
                 material={glassMaterial}
               />
-            </group>
+            </AnimatedBoat>
           </group>
           <group
             position={[69.93, -55.57, -44.55]}
