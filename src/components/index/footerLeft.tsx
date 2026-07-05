8c296ea3d86dba1a9b94c4612684505bb12b15c4
Richard Everley <hello@richardeverley.com>
2026-07-06 01:22:49 +1000

    refactor: use SplitText for FooterLeft animation

--- src/components/index/footerLeft.tsx
+++ src/components/index/footerLeft.tsx
@@ -1,44 +1,35 @@
 import React from "react";
-import gsap from "gsap";
 import styled from "styled-components";
 import layout from "../../styles/layout";
 import Route from "../../routes/route";
 import routeTo from "../../routes/routeTo";
-import gsapAnimationIndex from "../../helper/gsapAnimationIndex";
+import SplitText from "../motion/SplitText";
 import { INDEX_TO_ACKNOWLEDGEMENT_DESKTOP } from "../../constants/googleTags";
 
 const FooterLeft = ({
   setHover,
 }: {
   setHover: (value: React.SetStateAction<boolean>) => void;
-}) => {
-  const acknowledgementRef = React.useRef(null);
-
-  React.useEffect(() => {
-    gsap.defaults({ ease: "power4.out" });
-    gsap.from(acknowledgementRef.current, {
-      duration: 1,
-      ...gsapAnimationIndex(150, 1, 20),
-    });
-  }, []);
-
-  return (
-    <Container
-      ref={acknowledgementRef}
-      id={`${INDEX_TO_ACKNOWLEDGEMENT_DESKTOP}_0`}
+}) => (
+  <Container id={`${INDEX_TO_ACKNOWLEDGEMENT_DESKTOP}_0`}>
+    <h2
+      id={`${INDEX_TO_ACKNOWLEDGEMENT_DESKTOP}_1`}
+      className="font-secondary-normal select-none"
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
     >
-      <h2
-        id={`${INDEX_TO_ACKNOWLEDGEMENT_DESKTOP}_1`}
-        className="font-secondary-normal select-none"
+      <a
+        href={Route.Acknowledgement}
+        className="cursor-none"
         onClick={(e) => routeTo(e, Route.Acknowledgement)}
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
       >
-        Acknowledgement of Country
-      </h2>
-    </Container>
-  );
-};
+        <SplitText as="span" delay={1.2}>
+          Acknowledgement of Country
+        </SplitText>
+      </a>
+    </h2>
+  </Container>
+);
 
 export default FooterLeft;
 
