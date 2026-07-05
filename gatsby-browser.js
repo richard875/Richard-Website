30d5d1ea9f123df738c20183f13e82522917dad6
Richard Everley <hello@richardeverley.com>
2026-07-06 01:27:26 +1000

    feat: respect reduced motion and prevent icon flash

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,20 @@
 import React from "react";
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence, MotionConfig } from "framer-motion";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import "splitting/dist/splitting.css";
 import "./src/styles/global.scss";
 import "./src/styles/tailwind.css";
 import { MODE, STANDALONE } from "./src/constants/meta";
 
+// Icon CSS is imported above instead of being injected at runtime;
+// without this, SSR pages flash full-width icons on first paint.
+config.autoAddCss = false;
+
 export const wrapPageElement = ({ element }) => (
-  <AnimatePresence mode="wait">{element}</AnimatePresence>
+  <MotionConfig reducedMotion="user">
+    <AnimatePresence mode="wait">{element}</AnimatePresence>
+  </MotionConfig>
 );
 
 export const onServiceWorkerUpdateReady = () => {
