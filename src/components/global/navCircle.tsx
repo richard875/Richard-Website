aab70aa24beeb87f432730e04c2713d61eada091
Richard Everley <hello@richardeverley.com>
2026-07-13 02:30:28 +1000

    feat: add scale animation to NavCircle

--- src/components/global/navCircle.tsx
+++ src/components/global/navCircle.tsx
@@ -48,8 +48,8 @@ const NavCircle = ({
       >
         <CircleContainer
           id={`${tagId}_0`}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.4, delay, ease: [0, 0.71, 0.2, 1.01] }}
           onClick={(e) => routeTo(e, route, isDarkMode)}
           onMouseEnter={() => setHover(true)}
